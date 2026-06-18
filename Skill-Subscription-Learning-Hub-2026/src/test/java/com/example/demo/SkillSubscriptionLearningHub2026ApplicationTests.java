bb8dd317ad5807d5d89fb30704859925e6763866
Akshat-Singh29 <akshatksingh29@gmail.com>
2026-06-18 23:32:48 +0530

    Update SkillSubscriptionLearningHub2026ApplicationTests.java

--- Skill-Subscription-Learning-Hub-2026/src/test/java/com/example/demo/SkillSubscriptionLearningHub2026ApplicationTests.java
+++ Skill-Subscription-Learning-Hub-2026/src/test/java/com/example/demo/SkillSubscriptionLearningHub2026ApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class SkillSubscriptionLearningHub2026ApplicationTests {
+@SpringBootApplication
+public class SkillSubscriptionLearningHub2026Application {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSubscriptionLearningHub2026Application.class, args);
 	}
 
 }
